640c9d19e79c8bc8e451ab163d8ade37bdb5613a
DougLazyAngus <doug@lazyangus.com>
2015-06-05 17:15:35 -0700

    removed lights, flat materials."

--- LazyAngus/Assets/Scripts/PawController.cs
+++ LazyAngus/Assets/Scripts/PawController.cs
@@ -141,6 +141,8 @@ public class PawController : MonoBehaviour {
 	}
 
 	bool MoveTowards(Vector3 targetLocation) {
+		targetLocation.y = 0.0f;
+
 		Vector3 currrentLocation = transform.localPosition;
 		Vector3 direction = targetLocation - currrentLocation;
 		float deltaTime = Time.deltaTime;
