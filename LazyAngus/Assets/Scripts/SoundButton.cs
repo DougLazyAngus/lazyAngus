34e2ef098830151f72ef83fbd17bbcc4cc9eb8b5
DougLazyAngus <doug@lazyangus.com>
2015-07-01 18:47:51 -0700

    lost camera somehow

---
+++ LazyAngus/Assets/Scripts/SoundButton.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundButton : MonoBehaviour {
+	bool registeredForEvents;
+
+	Sprite soundOnSprite;
+	Sprite soundOffSprite;
+	public Image buttonImage;
+
+	void Awake() {
+		string path;
+
+		path = "Textures/Buttons/SoundIcon";
+		soundOnSprite = Resources.Load<UnityEngine.Sprite>(path);
+		
+		path = "Textures/Buttons/NoSoundIcon";
+		soundOffSprite = Resources.Load<UnityEngine.Sprite>(path);		
+	}
+
+	// Use this for initialization
+	void Start () {
+		RegisterForEvents ();	
+		UpdateButtonImage ();
+	}
+
+	void OnDestroy() {
+		UnregisterForEvents ();
+
+	}
+
+	void RegisterForEvents() {
+		if (registeredForEvents) {
+			return;
+		}
+		registeredForEvents = true;
+		SoundController.instance.SoundMuteChanged +=
+			new SoundController.SoundMuteChangedEventHandler (OnSoundMuteChanged);
+	}
+	
+	void UnregisterForEvents() {
+		if (registeredForEvents) {
+			SoundController.instance.SoundMuteChanged -=
+				new SoundController.SoundMuteChangedEventHandler (OnSoundMuteChanged);
+		}
+	}
+
+	void OnSoundMuteChanged() {
+		UpdateButtonImage ();
+	}
+
+	// Update is called once per frame
+	void UpdateButtonImage () {
+		if (SoundController.instance.soundMuted) {
+			buttonImage.sprite = soundOffSprite;
+		} else {
+			buttonImage.sprite = soundOnSprite;
+		}
+	}
+}
---
+++ LazyAngus/Assets/Scripts/SoundController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	public static SoundController instance;
+	public bool soundMuted { get; private set;}
+
+	public delegate void SoundMuteChangedEventHandler();
+	public event SoundMuteChangedEventHandler SoundMuteChanged;
+
+	
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ToggleSoundsMuted() {
+		soundMuted = !soundMuted;
+		if (SoundMuteChanged != null) {
+			SoundMuteChanged ();
+		}
+	}
+}
