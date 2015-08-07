5df01ae4cd6fc47061dc32fe216394143b2a7a1b
DougLazyAngus <doug@lazyangus.com>
2015-08-07 09:55:50 -0700

    constant size footer where ads go...

--- LazyAngus/Assets/Scripts/CameraController.cs
+++ LazyAngus/Assets/Scripts/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour {
 	public SizeCamera worldCamera;
 	public SizeCamera uxCamera;
 	public CanvasScaler constantUXCanvas;
+	public RectTransform footer;
 
 	public float actualHUDPixelHeight = 180f;
 	public float minHUDAspectRatio = 2.5f;
@@ -20,13 +21,16 @@ public class CameraController : MonoBehaviour {
 		uxCamera.screenPixelsTopToIgnore = 0f;
 		uxCamera.Configure ();
 
-		worldCamera.screenPixelsBottomToIgnore = GoogleAdController.GetBannerHeight ();
+
+		float footerHeight = GoogleAdController.GetBannerHeight ();
+		worldCamera.screenPixelsBottomToIgnore = footerHeight;
+		footer.sizeDelta = new Vector2( footer.rect.width, footerHeight);
 
 		float screenHeightPixels = Screen.height;
 		float screenWidthPixels = Screen.width;
 
 		// If we had a square world screen, how much would that leave for HUD?
-		float leftover = screenHeightPixels - worldCamera.screenPixelsBottomToIgnore - screenWidthPixels;
+		float leftover = screenHeightPixels - footerHeight - screenWidthPixels;
 
 		// Given screen width, what's the tallest HUD could be?
 		float maxHUDPixelHeight = screenWidthPixels / minHUDAspectRatio;
