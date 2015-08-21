11a47080a736983a6774f08a919d217b41ddbce7
DougLazyAngus <doug@lazyangus.com>
2015-08-21 13:13:29 -0700

    p

--- LazyAngus/Assets/Scripts/SocialMediaButtons.cs
+++ LazyAngus/Assets/Scripts/SocialMediaButtons.cs
@@ -8,19 +8,37 @@ public class SocialMediaButtons : MonoBehaviour {
 
 	public Button fbButton;
 	public Button twitterButton;
+	public Button shareButton;
 
 	// Use this for initialization
 	void Start () {
-		fbButton.onClick.AddListener(() => { 
-			int personalBest = PlayerStats.instance.GetHighScore ();
-			int finalScore = PlayerStats.instance.gameScore;
-			FacebookSharing.instance.ShareScore(finalScore, 
-			                                    finalScore == personalBest);
-		});
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			fbButton.gameObject.SetActive (false);
+			twitterButton.gameObject.SetActive (false);
+			shareButton.gameObject.SetActive (true);
 
-		twitterButton.onClick.AddListener(() => { 
-			int finalScore = PlayerStats.instance.gameScore;
-			TwitterSharing.instance.ShareScore(finalScore);
-		});
+			fbButton.onClick.AddListener (() => { 
+				int personalBest = PlayerStats.instance.GetHighScore ();
+				int finalScore = PlayerStats.instance.gameScore;
+				FacebookSharing.instance.ShareScore (finalScore, 
+				                                     finalScore == personalBest);
+			});
+		} else {
+			fbButton.gameObject.SetActive (true);
+			twitterButton.gameObject.SetActive (true);
+			shareButton.gameObject.SetActive (false);
+
+			fbButton.onClick.AddListener (() => { 
+				int personalBest = PlayerStats.instance.GetHighScore ();
+				int finalScore = PlayerStats.instance.gameScore;
+				FacebookSharing.instance.ShareScore (finalScore, 
+				                                    finalScore == personalBest);
+			});
+
+			twitterButton.onClick.AddListener (() => { 
+				int finalScore = PlayerStats.instance.gameScore;
+				TwitterSharing.instance.ShareScore (finalScore);
+			});
+		}
 	}
 }
