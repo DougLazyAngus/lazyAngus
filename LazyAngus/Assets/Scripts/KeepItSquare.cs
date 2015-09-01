73af61ed2c3b36fd4fdf32956c86a1482da3f189
DougLazyAngus <doug@lazyangus.com>
2015-09-01 09:53:40 -0700

    mucking with welcome screen.

---
+++ LazyAngus/Assets/Scripts/KeepItSquare.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepItSquare : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RectTransform rt = GetComponent<RectTransform> ();
+		if (rt) {
+			Rect r = rt.rect;
+			float height = r.height;
+			float width = r.width;
+			if (height > width) {
+				rt.localScale = new Vector2(height/width, 1);
+			} else {
+				rt.localScale = new Vector2(1, width/height);
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
