fc0fb249aa11c1b2c94f6cc6d954a1984a803cd3
DougLazyAngus <doug@lazyangus.com>
2015-09-20 15:29:50 -0700

    new assets.

---
+++ LazyAngus/Assets/Scripts/_Controllers/PausedCanvasController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PausedCanvasController : MonoBehaviour {
+	public Canvas pausedCanvas;
+
+	void Awake() {
+		if (pausedCanvas != null && 
+			!pausedCanvas.isActiveAndEnabled) {
+			pausedCanvas.gameObject.SetActive (true);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- LazyAngus/Assets/Scripts/_UIs/RealAngusButton.cs
+++ LazyAngus/Assets/Scripts/_UIs/RealAngusButton.cs
@@ -3,20 +3,62 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class RealAngusButton : MonoBehaviour {
+public class RealAngusButton : MonoBehaviour  {
 	public GameObject newLabel;
+	public RectTransform realAngusImageRectTransform;
+	public RectTransform rectTransform;
+
 	bool registeredForEvents;
+	float startTime;
+	float myHeight;
+	float myWidth;
+
+	public float waitingTime = 2;
+	public float risingTime = 2;
+	public float periodTime = 2;
+	public float deg = 10;
+
+	void Awake () {
+		rectTransform = GetComponent<RectTransform> ();
+		myHeight = rectTransform.rect.height;
+	}
+
 
 	// Use this for initialization
 	void Start () {
 		RegisterForEvents ();
 		UpdateDisplay ();
+		UpdateRealAngusImagePosition ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		UpdateRealAngusImagePosition ();
 	}
+
+	void UpdateRealAngusImagePosition() {
+		float delta = Time.time - startTime;
+
+		float yOffset = 0;
+
+		float imageRotation = 0;
+
+		delta -= waitingTime;
+
+		if (delta < risingTime) {
+			float fraction = delta / risingTime;
+			yOffset = Mathf.Lerp (-2 * myHeight, 0, fraction);
+		} else {
+			delta -= risingTime;
+			float rads = 2 * Mathf.PI * delta/periodTime;
+			float fraction = Mathf.Sin (rads);
+			imageRotation = fraction * deg;
+		}
+
+		rectTransform.anchoredPosition = new Vector2 (0, yOffset);
+		realAngusImageRectTransform.rotation = Quaternion.Euler (0, 0, imageRotation);
+	}
+
 
 	void OnDestroy() {
 		UnregisterForEvents ();
@@ -29,7 +71,7 @@ public class RealAngusButton : MonoBehaviour {
 		}
 		registeredForEvents = true;
 		GamePhaseState.instance.GamePhaseChanged += 
-			new GamePhaseState.GamePhaseChangedEventHandler (UpdateDisplay);
+			new GamePhaseState.GamePhaseChangedEventHandler (OnGamePhaseChanged);
 		RealAngusData.instance.RealAngusDataChanged += 
 			new RealAngusData.RealAngusDataChangedEventHandler (UpdateDisplay);
 	}
@@ -37,9 +79,18 @@ public class RealAngusButton : MonoBehaviour {
 	void UnregisterForEvents() {
 		if (registeredForEvents) {
 			GamePhaseState.instance.GamePhaseChanged -= 
-				new GamePhaseState.GamePhaseChangedEventHandler (UpdateDisplay);
+				new GamePhaseState.GamePhaseChangedEventHandler (OnGamePhaseChanged);
 			RealAngusData.instance.RealAngusDataChanged -= 
 				new RealAngusData.RealAngusDataChangedEventHandler (UpdateDisplay);
+		}
+	}
+
+	void OnGamePhaseChanged() {
+		UpdateDisplay ();
+
+		if (GamePhaseState.instance.previousGamePhase != GamePhaseState.GamePhaseType.REAL_ANGUS) {
+			startTime = Time.time;
+			UpdateRealAngusImagePosition();
 		}
 	}
 
