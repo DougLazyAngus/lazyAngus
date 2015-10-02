670dd29a8b6c9d662e5f888b851bddaf57d80fb3
DougLazyAngus <doug@lazyangus.com>
2015-10-02 11:59:55 -0700

    trying to get android ads working.  removed all plugins and reinstalled.  game works now, at least in editor.

---
+++ LazyAngus/Assets/Extensions/GooglePlayCommon/Editor/SocialPlatfromInit.cs
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+//  
+// @module V2D
+// @author Osipov Stanislav lacost.st@gmail.com
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[InitializeOnLoad]
+public class SocialPlatfromInit  {
+
+	static SocialPlatfromInit () {
+
+		if(FileStaticAPI.IsFileExists("Extensions/UltimateMobile/Resources/UltimateMobileSettings.asset")) {
+			return;
+		}
+
+		if(FileStaticAPI.IsFileExists("Extensions/AndroidNative/Resources/AndroidNativeSettings.asset")) {
+			return;
+		}
+
+
+		if(!SocialPlatfromSettingsEditor.IsInstalled) {
+			EditorApplication.update += OnEditorLoaded;
+		} else {
+			if(!SocialPlatfromSettingsEditor.IsUpToDate) {
+				EditorApplication.update += OnEditorLoaded;
+			}
+		}
+		
+	}
+	
+	private static void OnEditorLoaded() {
+		
+		EditorApplication.update -= OnEditorLoaded;
+		Debug.LogWarning("Mobile Social Plugin Install Required. Opening Plugin settings...");
+		Selection.activeObject = SocialPlatfromSettings.Instance;
+	}
+}
